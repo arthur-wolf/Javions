0114636cc4dff3f4f907e59c2acc2cb4c6c12dd4
Oussama <oussama.ghali@epfl.ch>
2023-02-21 09:30:39 +0100

    Add Units Class

---
+++ src/ch/epfl/javions/Units.java
@@ -0,0 +1,50 @@
+package ch.epfl.javions;
+
+public final class Units {
+
+    private Units(){}
+    public static final double CENTI = 1e-2;
+    public static final double KILO = 0;
+
+    public static class Angle {
+
+        private Angle (){}
+        public static final double RADIAN = 1;
+        public static final double TURN = 2 * Math.PI;
+        public static final double DEGREE = TURN/360;
+        public static final double T32 = Math.scalb(TURN,-32);
+    }
+    public static class Length{
+
+        private Length (){}
+        public static final double METER = 1;
+        public static final double CENTIMETER = CENTI * METER;
+        public static final double KILOMETER = CENTI * CENTIMETER;
+        public static final double INCH = 2.54;
+        public static final double FOOT = 12;
+        public static final double NAUTICAL_MILE = 1852;
+
+    }
+    public static class Time {
+        private Time (){}
+        public static final double SECOND = 1;
+        public static final double MINUTE = 60;
+        public static final double HOUR = 60;
+
+    }
+    public static class Speed {
+        private Speed (){}
+        public static final double KNOT = 1;
+        public static final double KILOMETER_PER_HOUR = 1;
+
+    }
+    static double convert(double value, double fromUnit, double toUnit){
+        return value * (fromUnit/toUnit);
+    }
+    static double convertFrom(double value, double fromUnit){
+        return value * (fromUnit);
+    }
+    static double convertTo(double value, double toUnit){
+        return value * (toUnit);
+    }
+}
