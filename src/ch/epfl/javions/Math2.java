5a8812a71e8e30229829a2474d964c1e37a364e7
Arthur Wolf <arthur.wlf@icloud.com>
2023-02-21 09:03:38 +0100

    Add Math2 class

---
+++ src/ch/epfl/javions/Math2.java
@@ -0,0 +1,19 @@
+package ch.epfl.javions;
+
+public final class Math2 {
+    private Math2() {}
+
+    public static int clamp(int min, int v, int max) {
+        Preconditions.checkArgument(min <= max);
+        if (v < min) {
+            return min;
+        } else if (v > max) {
+            return max;
+        }
+        return v;
+    }
+
+    public static double asinh(double x) {
+        return Math.log(x + Math.sqrt(1 + Math.pow(x, 2)));
+    }
+}
