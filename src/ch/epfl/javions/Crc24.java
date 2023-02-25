75a15bce45b0d4f0659a8b24b5558f0606d467d9
Oussama <oussama.ghali@epfl.ch>
2023-02-25 12:06:12 +0100

    Add the skelet of Crc24 Class

---
+++ src/ch/epfl/javions/Crc24.java
@@ -0,0 +1,21 @@
+package ch.epfl.javions;
+
+/**
+ * @author Arthur Wolf (344200)
+ * @author Oussama Ghali (341478)
+ */
+
+public final class Crc24 {
+    public Crc24(int generator) {
+        //todo : returns a CRC24 calculator using the generator whose 24 least significant bits are those of generator.
+        return;
+    }
+
+    public static int generator;
+
+    public int crc(byte[] bytes) {
+        //todo : returns the CRC24 of the given array
+        return 0;
+    }
+
+}
