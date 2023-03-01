823b245e05f14568ec384864d9e99d8209d44bb3
Arthur Wolf <arthur.wlf@icloud.com>
2023-03-01 15:16:57 +0100

    Finish Crc24 and its tests

--- src/ch/epfl/javions/Crc24.java
+++ src/ch/epfl/javions/Crc24.java
@@ -1,50 +1,83 @@
 package ch.epfl.javions;
 
 /**
+ * Represents a CRC24 checksum
+ *
  * @author Arthur Wolf (344200)
  * @author Oussama Ghali (341478)
  */
 
 public final class Crc24 {
 
-    public static final int GENERATOR = 0xFF409;
-    private final byte[] table;
+    private static final int GENERATOR_LENGTH = 24;
+    public static final int GENERATOR = 0xFFF409;
+    private final int[] table;
 
-    public Crc24(int generator){
-       // Crc24 crc = new Crc24(Crc24.GENERATOR);
-        this.table = buildTable();
+    /**
+     * Builds the table used to compute the CRC24 checksum
+     *
+     * @param generator the generator to use
+     */
+    public Crc24(int generator) {
+        this.table = buildTable(generator);
     }
 
-    //dont pass the test
+    /**
+     * Computes the CRC24 checksum of the given bytes
+     *
+     * @param bytes the bytes to compute the checksum of
+     * @return the checksum of the given bytes
+     */
     public int crc(byte[] bytes) {
         int crc = 0;
-        for (byte o : bytes) {
-            //need to change crc >> 16 i guess
-            crc = ((crc << 8) | o) ^ table[(crc >> 16) & 0xFF];
-            System.out.println("crc" + crc);
-        }
-        return crc & 0xFFFFFF;
-    }
-    private static int crc_bitwise(int generator, byte [] bytes){
-        int crc = 0;
-        int[] table = new int[]{0, generator};
         for (byte b : bytes) {
-            //look good to me
-            crc = ((crc << 1) | b) ^ (table[crc >> 23]);
-            System.out.println("crc_bitwise " + crc);
+            crc = ((crc << Byte.SIZE) | Byte.toUnsignedInt(b)) ^ table[Bits.extractUInt(crc, GENERATOR_LENGTH - 8, 8)];
         }
-        return crc & 0xFFFFFF;
 
+        for (int i = 0; i < 24; i++) {
+            crc = (crc << 1) ^ (table[Bits.extractUInt(crc, GENERATOR_LENGTH - 1, 1)]);
+        }
+
+        // Return the 24 least significant bits
+        return Bits.extractUInt(crc, 0, GENERATOR_LENGTH);
     }
-    //look good to me
-    private static byte[]  buildTable(){
-        byte[] table = new byte[256];
+
+    /**
+     * Computes the CRC24 checksum of the given bytes using a bitwise algorithm
+     *
+     * @param generator the generator to use
+     * @param bytes     the bytes to compute the checksum of
+     * @return the CRC24 checksum of the given bytes
+     */
+    public static int crc_bitwise(int generator, byte[] bytes) {
+        int[] table = new int[]{0, generator};
+        int crc = 0;
+
+        for (byte b : bytes) {
+            for (int i = 7; i >= 0; i--) {
+                crc = ((crc << 1) | Bits.extractUInt(b, i, 1)) ^ (table[Bits.extractUInt(crc, GENERATOR_LENGTH - 1, 1)]);
+            }
+        }
+
+        for (int i = 0; i < 24; i++) {
+            crc = (crc << 1) ^ (table[Bits.extractUInt(crc, GENERATOR_LENGTH - 1, 1)]);
+        }
+
+        // Return the 24 least significant bits
+        return Bits.extractUInt(crc, 0, GENERATOR_LENGTH);
+    }
+
+    /**
+     * Builds the table for the given generator
+     *
+     * @param generator the given generator
+     * @return the table built according to the given generator
+     */
+    private static int[] buildTable(int generator) {
+        int[] table = new int[256];
         for (int i = 0; i < 256; i++) {
-            table[i] = (byte) crc_bitwise(GENERATOR, new byte[]{(byte) i});
+            table[i] = crc_bitwise(generator, new byte[]{(byte) i});
         }
         return table;
-
     }
-
-
 }
--- test/ch/epfl/javions/Crc24Test.java
+++ test/ch/epfl/javions/Crc24Test.java
@@ -9,11 +9,81 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Crc24Test {
     @Test
-    public void testCrc24() {
+    public void crc24WorksOnTrivialInput1() {
         Crc24 crc24 = new Crc24(Crc24.GENERATOR);
         String mS = "8D392AE499107FB5C00439";
         String cS = "035DB8";
-        int c = Integer.parseInt(cS, 16); // == 0x035DB8
+        int c = Integer.parseInt(cS, 16); // 0x035DB8
+
+        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+        assertEquals(0, crc24.crc(mAndC));
+
+        byte[] mOnly = HexFormat.of().parseHex(mS);
+        assertEquals(c, crc24.crc(mOnly));
+    }
+
+    @Test
+    public void crc24WorksOnTrivialInput2() {
+        Crc24 crc24 = new Crc24(Crc24.GENERATOR);
+        String mS = "8D4D2286EA428867291C08";
+        String cS = "EE2EC6";
+        int c = Integer.parseInt(cS, 16); // 0xEE2EC6
+
+        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+        assertEquals(0, crc24.crc(mAndC));
+
+        byte[] mOnly = HexFormat.of().parseHex(mS);
+        assertEquals(c, crc24.crc(mOnly));
+    }
+
+    @Test
+    public void crc24WorksOnTrivialInput3() {
+        Crc24 crc24 = new Crc24(Crc24.GENERATOR);
+        String mS = "8D3950C69914B232880436";
+        String cS = "BC63D3";
+        int c = Integer.parseInt(cS, 16); // 0xBC63D3
+
+        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+        assertEquals(0, crc24.crc(mAndC));
+
+        byte[] mOnly = HexFormat.of().parseHex(mS);
+        assertEquals(c, crc24.crc(mOnly));
+    }
+
+    @Test
+    public void crc24WorksOnTrivialInput4() {
+        Crc24 crc24 = new Crc24(Crc24.GENERATOR);
+        String mS = "8D4B17E399893E15C09C21";
+        String cS = "9FC014";
+        int c = Integer.parseInt(cS, 16); // 0x9FC014
+
+        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+        assertEquals(0, crc24.crc(mAndC));
+
+        byte[] mOnly = HexFormat.of().parseHex(mS);
+        assertEquals(c, crc24.crc(mOnly));
+    }
+
+    @Test
+    public void crc24WorksOnTrivialInput5() {
+        Crc24 crc24 = new Crc24(Crc24.GENERATOR);
+        String mS = "8D4B18F4231445F2DB63A0";
+        String cS = "DEEB82";
+        int c = Integer.parseInt(cS, 16); // 0xDEEB82
+
+        byte[] mAndC = HexFormat.of().parseHex(mS + cS);
+        assertEquals(0, crc24.crc(mAndC));
+
+        byte[] mOnly = HexFormat.of().parseHex(mS);
+        assertEquals(c, crc24.crc(mOnly));
+    }
+
+    @Test
+    public void crc24WorksOnTrivialInput6() {
+        Crc24 crc24 = new Crc24(Crc24.GENERATOR);
+        String mS = "8D495293F82300020049B8";
+        String cS = "111203";
+        int c = Integer.parseInt(cS, 16); // 0x111203
 
         byte[] mAndC = HexFormat.of().parseHex(mS + cS);
         assertEquals(0, crc24.crc(mAndC));
