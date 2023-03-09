73f2ab982bbaa9cbdd74cbfdc956c340c3e82099
Oussama <oussama.ghali@epfl.ch>
2023-03-09 10:42:57 +0100

    Continue PowerWindow

--- src/ch/epfl/javions/demodulation/PowerWindow.java
+++ src/ch/epfl/javions/demodulation/PowerWindow.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- *
  * @author Oussama Ghali (341478)
  * @author Arthur Wolf (344200)
  */
@@ -14,13 +13,13 @@ import java.io.InputStream;
 public final class PowerWindow {
 
     private final InputStream inputStream;
-    private int windowSize;
-    private int[] evenTab;
-    private int[] oddTab;
+    private byte[] evenTab;
+    private byte[] oddTab;
 
-    private long count = 0;
+    private final double batchSize = Math.pow(2, 16);
+    private int windowSize = (int) Math.pow(2, 16);
+    private long position = 0;
 
-    private
 
     /**
      * Constructs a new power window
@@ -30,22 +29,32 @@ public final class PowerWindow {
      * @throws IOException if an I/O error occurs
      */
 
-        //todo make the constructor public
-    PowerWindow(InputStream stream, int windowSize) throws IOException {
+    public PowerWindow(InputStream stream, int windowSize) throws IOException {
         Preconditions.checkArgument(windowSize > 0 && windowSize <= Math.pow(2, 16));
         this.inputStream = stream;
         this.windowSize = windowSize;
+        this.evenTab = new byte[windowSize * 2];
+        this.oddTab = new byte[windowSize * 2];
 
     }
 
+    /**
+     * @return the size of the window
+     */
     public int size() {
         return windowSize;
     }
 
-    public long position() throws IOException {
-        return count;
+    /**
+     * @return the current position of the window
+     */
+    public long position() {
+        return position;
     }
 
+    /**
+     * @return always true except when the end of the sample stream has been reached, and the window passes it
+     */
     public boolean isFull() {
         if (windowSize > Math.pow(2, 16)) {
             return false;
@@ -53,18 +62,39 @@ public final class PowerWindow {
         return true;
     }
 
+    /**
+     * @param i the index of the sample to get
+     * @return returns the power sample at the given index
+     * @throws IOException if i is not in the range [0, windowSize[
+     */
+
     public int get(int i) throws IOException {
         Preconditions.checkArgument(i >= 0 && i < windowSize);
         //todo
         return i;
     }
 
+    /**
+     * Advances the window by one sample
+     *
+     * @throws IOException if windowSize is greater than 2^16
+     */
     public void advance() throws IOException {
-        windowSize++;
         Preconditions.checkArgument(windowSize <= Math.pow(2, 16));
-        count++;
+
+        for (int i = 0; i < oddTab.length; i++) {
+
+
+        }
+        position++;
     }
 
+    /**
+     * Works as if we were calling advance() offset times
+     *
+     * @param offset the number of samples to advance by
+     * @throws IOException if these number (offset) is equal or less than 0
+     */
     public void advanceBy(int offset) throws IOException {
         Preconditions.checkArgument(offset > 0);
         for (int i = 0; i < offset; i--) {
