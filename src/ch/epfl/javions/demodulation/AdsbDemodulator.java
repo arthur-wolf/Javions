d3a7bbe800566a51028b955503b4427b1575c5a3
Arthur Wolf <arthur.wlf@icloud.com>
2023-03-15 14:56:06 +0100

    Start AdsbDemodulator

--- src/ch/epfl/javions/demodulation/AdsbDemodulator.java
+++ src/ch/epfl/javions/demodulation/AdsbDemodulator.java
@@ -1,6 +1,5 @@
 package ch.epfl.javions.demodulation;
 
-import ch.epfl.javions.Preconditions;
 import ch.epfl.javions.adsb.RawMessage;
 
 import java.io.IOException;
@@ -11,21 +10,16 @@ import java.io.InputStream;
  * @author Oussama Ghali (341478)
  */
 public final class AdsbDemodulator {
-
-    private final InputStream samplesStream;
     private final PowerWindow powerWindow;
 
     /**
      * Constructs a new ADS-B demodulator
      *
-     * @param samplesStram the stream of samples
+     * @param samplesStream the stream of samples
      * @throws IOException if an I/O error occurs
      */
-    public AdsbDemodulator(InputStream samplesStram) throws IOException {
-        this.samplesStream = samplesStram;
+    public AdsbDemodulator(InputStream samplesStream) throws IOException {
         this.powerWindow = new PowerWindow(samplesStream, 1200);
-
-
     }
 
     /**
@@ -35,6 +29,40 @@ public final class AdsbDemodulator {
      * @throws IOException if an I/O error occurs
      */
     public RawMessage nextMessage() throws IOException {
+        int sigmaP = 0;
+        int sigmaP1 = 0;
+        int sigmaP_1;
+        int sigmaV;
+
+        while (powerWindow.isFull()) {
+            sigmaP_1 = sigmaP;
+            sigmaP = sigmaP1;
+            sigmaV = powerWindow.get(5) + powerWindow.get(15) + powerWindow.get(20) + powerWindow.get(25) + powerWindow.get(30) + powerWindow.get(40);
+            sigmaP1 = powerWindow.get(1) + powerWindow.get(11) + powerWindow.get(36) + powerWindow.get(46);
+
+
+            // If this condition is verified, we consider that the preamble of the message is indeed at the beginning of the window
+            if (sigmaP >= 2 * sigmaV) {
+                if ((sigmaP > sigmaP1) && (sigmaP_1 < sigmaP)) {
+                    byte[] bytes = new byte[14];
+                        for (int i = 0; i < 8; i++) {
+                            bytes[0] |= (powerWindow.get(80 + 10 * i) < powerWindow.get(85 + 10 * i) ? 0 : 1) << (7 - i % 8);
+                        }
+                        if (RawMessage.size(bytes[0]) == 14) {
+                            for (int i = 8; i < 112; i++) {
+                                bytes[i / 8] |= (powerWindow.get(80 + 10 * i) < powerWindow.get(85 + 10 * i) ? 0 : 1) << (7 - i % 8);
+                            }
+                            RawMessage rawMessage = RawMessage.of(powerWindow.position() * 100, bytes);
+
+                            if (rawMessage != null) {
+                                powerWindow.advanceBy(1200);
+                                return rawMessage;
+                            }
+                        }
+                }
+            }
+            powerWindow.advance();
+        }
         return null;
     }
 
---
+++ test/ch/epfl/javions/demodulation/PrintRawMessagesTest.java
@@ -0,0 +1,21 @@
+package ch.epfl.javions.demodulation;
+
+import ch.epfl.javions.adsb.RawMessage;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public final class PrintRawMessagesTest {
+    @Test
+void Test() throws IOException  {
+        String f = "resources/samples_20230304_1442.bin";
+        try (InputStream s = new FileInputStream(f)) {
+            AdsbDemodulator d = new AdsbDemodulator(s);
+            RawMessage m;
+            while ((m = d.nextMessage()) != null)
+                System.out.println(m);
+        }
+    }
+}
