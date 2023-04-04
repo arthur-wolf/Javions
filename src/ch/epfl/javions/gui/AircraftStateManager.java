d8766a2d6c4628784241856f3b2227c6561c61bf
Arthur Wolf <arthur.wlf@icloud.com>
2023-04-04 08:36:12 +0200

    Add GUI skeleton to the project

---
+++ src/ch/epfl/javions/gui/AircraftStateManager.java
@@ -0,0 +1,5 @@
+package ch.epfl.javions.gui;
+
+public final class AircraftStateManager {
+
+}
---
+++ src/ch/epfl/javions/gui/ObservableAircraftState.java
@@ -0,0 +1,47 @@
+package ch.epfl.javions.gui;
+
+import ch.epfl.javions.GeoPos;
+import ch.epfl.javions.adsb.AircraftStateSetter;
+import ch.epfl.javions.adsb.CallSign;
+
+public final class ObservableAircraftState implements AircraftStateSetter {
+
+    public ObservableAircraftState() {
+        //todo
+    }
+
+    @Override
+    public void setLastMessageTimeStampNs(long timeStampNs) {
+        //todo
+    }
+
+    @Override
+    public void setCategory(int category) {
+        //todo
+    }
+
+    @Override
+    public void setCallSign(CallSign callSign) {
+        //todo
+    }
+
+    @Override
+    public void setPosition(GeoPos position) {
+        //todo
+    }
+
+    @Override
+    public void setAltitude(double altitude) {
+        //todo
+    }
+
+    @Override
+    public void setVelocity(double velocity) {
+        //todo
+    }
+
+    @Override
+    public void setTrackOrHeading(double trackOrHeading) {
+        //todo
+    }
+}
